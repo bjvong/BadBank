ec02b0c8a886710fea90e89edc9e65561be04546
Beau VonGunten <bjvong@gmail.com>
2023-05-07 12:44:02 -0400

    testing stuff

--- public/index.js
+++ public/index.js
@@ -1,36 +1,29 @@
 function Spa(){
-    const url = '/currentUser';
-    ( async ()=>{
-       var res = await fetch(url);
-       var data = await res.json();
-       console.log(data);
-    })();
+ const [loggedIn, setLoggedIn] = useState(false);
+ const SecuredRoute = ({ ...props }) => (
+        console.log(props.path),
+        <Route path={props.path} render={(data) => (
+          console.log(data),
+          localStorage.getItem('accessToken')
+            ? <props.render {...data} />
+            : <Redirect to='/login' />
+        )} />
+      )
         
-    if(data.email){
+
     return(
         <HashRouter>
-            <NavBar/>
+            <NavBar loggedIn={loggedIn}/>
             <UserContext.Provider value={{users:[{name:'beau', email:'beau@gmail.com',password:'secret',balance:100.00}]}}>
                 <Route path="/" exact component={Home} />
+                <Route path="/Login/" exact component={() => <Login setLoggedIn={setLoggedIn} />} />
                 <Route path="/CreateAccount/" exact component={CreateAccount} />
-                <Route path="/deposit/" exact component={Deposit} />
+                <SecuredRoute path="/deposit/" exact component={Deposit} />
                 <Route path="/withdraw/" exact component={Withdraw} />
                 <Route path="/alldata/" exact component={AllData} />
             </UserContext.Provider>
         </HashRouter>
     );
-    }else{
-    return(
-    <HashRouter>
-        <NavBar/>
-        <UserContext.Provider value={{users:[{name:'beau', email:'beau@gmail.com',password:'secret',balance:100.00}]}}>
-            <Route path="/" exact component={Home} />
-            <Route path="/CreateAccount/" exact component={CreateAccount} />
-        </UserContext.Provider>
-    </HashRouter>
-    );
-    }
-
 }
 
 
--- public/navbar.js
+++ public/navbar.js
@@ -1,5 +1,6 @@
-function NavBar(){
+function NavBar(loggedIn){
     return (
+      loggedIn ? 
         <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
@@ -9,8 +10,28 @@ function NavBar(){
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link" data-toggle="tooltip" title="Create an new account Here!" href="#/CreateAccount/">Create Account</a>
-        <a className="nav-link" data-toggle="tooltip" title="Login to your Bad Bank" href="#/login/">Log in</a>
+        {/* <a className="nav-link" data-toggle="tooltip" title="Create an new account Here!" href="#/CreateAccount/">Create Account</a>
+        <a className="nav-link" data-toggle="tooltip" title="Login to your Bad Bank" href="#/login/">Log in</a> */}
+        <a className="nav-link" data-toggle="tooltip" title="Make a deposit to your account!" href="#/deposit/">Deposit</a>
+        <a className="nav-link" data-toggle="tooltip" title="Make a withdraw from your account!" href="#/withdraw/">Withdraw</a>
+        <a className="nav-link" data-toggle="tooltip" title="View all data" href="#/alldata/">AllData</a>
+      </div>
+    </div>
+  </div>
+</nav>
+        </>
+        :
+        <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">BadBank</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        {/* <a className="nav-link" data-toggle="tooltip" title="Create an new account Here!" href="#/CreateAccount/">Create Account</a>
+        <a className="nav-link" data-toggle="tooltip" title="Login to your Bad Bank" href="#/login/">Log in</a> */}
         <a className="nav-link" data-toggle="tooltip" title="Make a deposit to your account!" href="#/deposit/">Deposit</a>
         <a className="nav-link" data-toggle="tooltip" title="Make a withdraw from your account!" href="#/withdraw/">Withdraw</a>
         <a className="nav-link" data-toggle="tooltip" title="View all data" href="#/alldata/">AllData</a>
