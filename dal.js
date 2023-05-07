d14b74a4acd4c8d75a567986b3d7a8bfa9e8495e
Beau VonGunten <bjvong@gmail.com>
2023-05-06 22:31:30 -0400

    toying

--- dal.js
+++ dal.js
@@ -1,16 +1,16 @@
 // const { MongoClient } = require("mongodb");
 // const uri = 'mongodb+srv://bjvong:secret123@bad-bank-bjv.iyiafcn.mongodb.net/?retryWrites=true&w=majority';
+
 const mongoose = require("mongoose");
+const port = process.env.PORT || 8050;
+const db = process.env.MONGO_URI;
+mongoose.connect(db);
 const bcrypt = require("bcrypt");
+const UserService = require("./src/user");
 
 require("./src/config/google");
 require("./src/config/local");
 require("./src/config/passport");
-
-
-const port = process.env.PORT || 8050;
-const db = process.env.MONGO_URI;
-mongoose.connect(db);
 
 async function createUser( name, email, password){
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -24,31 +24,43 @@ async function createUser( name, email, password){
         })
       } catch (e) {
         req.flash("error", "Error creating a new account. Try a different login method.");
-        res.redirect("/CreateAccount")
+        
       }
-    
-      res.redirect("/")
     }
 
 
-
-// async function all(){
-//     const dbName = 'myProject';
-//     const collection = 'users';
+// async function createUser( name, email, password){
 //     const client = new MongoClient(uri);
 //     try{
-//     await client.connect();
-//     const databasesList = client.db(dbName).collection(collection).find();
-//     const results = await databasesList.toArray();
-//     return(results);
+//         await client.connect();
+//         const dbName = 'myProject';
+//         const db = client.db(dbName);
+//         var collection = db.collection('users');
+//         var doc = {name, email, password, balance: 0};
+//         collection.insertOne(doc, {w:1}, function(err, result) {
+//             console.log('Data inserted');
+//         });
 //     }catch (e) {
 //         console.error(e);
 //     }
+
+// }
+
+async function all(){
+    const dbName = 'myProject';
+    const collection = 'users';
+    const client = new MongoClient(uri);
+    try{
+    await client.connect();
+    const databasesList = client.db(dbName).collection(collection).find();
+    const results = await databasesList.toArray();
+    return(results);
+    }catch (e) {
+        console.error(e);
+    }
     
-// };
+};
 
 
-// module.exports = {createUser,all};
-
-module.exports = {createUser};
+module.exports = {createUser,all};
  
