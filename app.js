17679d43d369aa8ef590d85545e3fba874281f4d
Beau VonGunten <bjvong@gmail.com>
2023-05-07 10:30:26 -0400

    think i found it

--- app.js
+++ app.js
@@ -89,9 +89,9 @@ app.get("/auth/logout", (req, res) => {
 //     res.render("local/signin.ejs");
 //   });
 
- app.get("/profile", isLoggedIn, (req, res) => {
+ app.get("/", isLoggedIn, (req, res) => {
      console.log(req.user);
-     res.render("./index.html");
+     res.redirect("/");
    });
 
 app.get('/auth/local/signup/:firstName/:lastName/:email/:password', function (req, res) {   
