652c091bd7db0729e90161d432724bc429d5ef6b
Beau VonGunten <bjvong@gmail.com>
2023-05-07 13:06:45 -0400

    silly mistake

--- public/navbar.js
+++ public/navbar.js
@@ -13,10 +13,10 @@ function NavBar(loggedIn){
       <div className="navbar-nav">
         <a className="nav-link" data-toggle="tooltip" title="Create an new account Here!" href="#/CreateAccount/">Create Account</a>
         <a className="nav-link" data-toggle="tooltip" title="Login to your Bad Bank" href="#/login/">Log in</a>
-        <a className="nav-link" data-toggle="tooltip" title="Log out of your account" href="/auth/logout">Log Out</a>
+        {/* <a className="nav-link" data-toggle="tooltip" title="Log out of your account" href="/auth/logout">Log Out</a>
         <a className="nav-link" data-toggle="tooltip" title="Make a deposit to your account!" href="#/Deposit/">Deposit</a>
         <a className="nav-link" data-toggle="tooltip" title="Make a withdraw from your account!" href="#/Withdraw/">Withdraw</a>
-        <a className="nav-link" data-toggle="tooltip" title="View all data" href="#/AllData/">AllData</a>
+        <a className="nav-link" data-toggle="tooltip" title="View all data" href="#/AllData/">AllData</a> */}
       </div>
     </div>
   </div>
@@ -32,8 +32,9 @@ function NavBar(loggedIn){
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link" data-toggle="tooltip" title="Create an new account Here!" href="#/CreateAccount/">Create Account</a>
-        <a className="nav-link" data-toggle="tooltip" title="Login to your Bad Bank" href="#/Login/">Log in</a>
+        {/* <a className="nav-link" data-toggle="tooltip" title="Create an new account Here!" href="#/CreateAccount/">Create Account</a>
+        <a className="nav-link" data-toggle="tooltip" title="Login to your Bad Bank" href="#/Login/">Log in</a> */}
+        <a className="nav-link" data-toggle="tooltip" title="Log out of your account" href="/auth/logout">Log Out</a>
         <a className="nav-link" data-toggle="tooltip" title="Make a deposit to your account!" href="#/Deposit/">Deposit</a>
         <a className="nav-link" data-toggle="tooltip" title="Make a withdraw from your account!" href="#/Withdraw/">Withdraw</a>
         <a className="nav-link" data-toggle="tooltip" title="View all data" href="#/AllData/">AllData</a>
