076fe7503ec38a2286a34f2b2dd768f017679997
Beau VonGunten <bjvong@gmail.com>
2023-05-07 12:47:10 -0400

    testing stuffs

--- public/index.js
+++ public/index.js
@@ -1,14 +1,5 @@
 function Spa(){
- const [loggedIn, setLoggedIn] = useState(false);
- const SecuredRoute = ({ ...props }) => (
-        console.log(props.path),
-        <Route path={props.path} render={(data) => (
-          console.log(data),
-          localStorage.getItem('accessToken')
-            ? <props.render {...data} />
-            : <Redirect to='/login' />
-        )} />
-      )
+ const [loggedIn, setLoggedIn] = React.useState(false);
         
 
     return(
@@ -18,7 +9,7 @@ function Spa(){
                 <Route path="/" exact component={Home} />
                 <Route path="/Login/" exact component={() => <Login setLoggedIn={setLoggedIn} />} />
                 <Route path="/CreateAccount/" exact component={CreateAccount} />
-                <SecuredRoute path="/deposit/" exact component={Deposit} />
+                <Route path="/deposit/" exact component={Deposit} />
                 <Route path="/withdraw/" exact component={Withdraw} />
                 <Route path="/alldata/" exact component={AllData} />
             </UserContext.Provider>
